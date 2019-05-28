cafa862dc312bb1079a6ba60f4b4dd1237bcfec1
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-28 00:29:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,11 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   
   
-  name_hash.each do |key,value|
-    if lowest_value == 0 || value < lowest_value
-      lowest_value = value 
-      lowest_key = key
+  name_hash.each do |name,price|
+    if lowest == 0 || price< lowest_value
+      lowest = price
+      a = name
     end
   end
-  lowest_key
+  a
 end
