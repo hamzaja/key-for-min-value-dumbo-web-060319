76375d1a3d3bf36a979779e14c22fe1f9eb0432b
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-28 00:30:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   lowest_value = 0
-  
+  a = nil
   
   name_hash.each do |name,price|
     if lowest == 0 || price< lowest_value
